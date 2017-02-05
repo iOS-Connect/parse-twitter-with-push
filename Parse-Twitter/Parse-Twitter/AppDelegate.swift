c23ba06830534a42303834e65c7caff15ccf4333
yhu98 <yhu98@ucsc.edu>
2017-02-05 13:57:32 -0800

    more pod installed

--- Parse-Twitter/Parse-Twitter/AppDelegate.swift
+++ Parse-Twitter/Parse-Twitter/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let config = ParseClientConfiguration {
+            $0.applicationId = "myAppId"
+            $0.clientKey = "myMasterKey"
+            $0.server = "https://iostwitter.herokuapp.com/parse"
+            
+        }
+        Parse.initialize(with: config)
+        
         return true
     }
 
