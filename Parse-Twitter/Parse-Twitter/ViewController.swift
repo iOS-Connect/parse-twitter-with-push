66bfed61576a59a7dc2f6baf320c0779159e3100
John Regner <regnerjr@gmail.com>
2017-02-05 20:49:29 -0800

    Remove comments and cleanup whitespace

--- Parse-Twitter/Parse-Twitter/ViewController.swift
+++ Parse-Twitter/Parse-Twitter/ViewController.swift
@@ -1,18 +1,9 @@
-//
-//  ViewController.swift
-//  Parse-Twitter
-//
-//  Created by John Regner on 2/5/17.
-//  Copyright © 2017 Santa Clara iOS Connect. All rights reserved.
-//
-
 import UIKit
 import Parse
 import ParseUI
 
 class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
-    
-    
+
     @IBOutlet var loginButton: UIButton!
     
     func show(_ sender: Any) {
@@ -22,60 +13,41 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpV
         self.navigationController?.pushViewController(ctvc, animated: true)
         //present(ctvc, animated: true, completion: nil)
     }
-  
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-    }
     
     @IBAction func loginButtonClicked(_ sender: Any) {
-        
         let pfLoginPage = PFLogInViewController()
         pfLoginPage.delegate = self
         pfLoginPage.signUpController?.delegate = self
         self.present(pfLoginPage, animated: true) { 
             print("pfloginpage presented")
         }
-        
-        
-        
     }
     
     func signUpViewController(_ signUpController: PFSignUpViewController, didSignUp user: PFUser) {
-        
         print("success")
         print(user)
-        
-        //present homeViewControler Here
-        
     }
-    
-    
+
     func signUpViewController(_ signUpController: PFSignUpViewController, didFailToSignUpWithError error: Error?) {
         if let er = error {
             print(er.localizedDescription)
         }
-        
     }
-  
+
     func log(_ logInController: PFLogInViewController, didFailToLogInWithError error: Error?) {
-        
         if let er = error {
             print(er.localizedDescription)
         }
     }
-    
+
     func log(_ logInController: PFLogInViewController, didLogIn user: PFUser) {
-        //dismiss top login VC
+
         logInController.dismiss(animated: true, completion: nil)
         print(user.email!)
-        
+
         if let delegate = UIApplication.shared.delegate as? AppDelegate {
             delegate.appController.didLogin()
         }
-        //present homeViewControler Here
      }
-
 }
 
